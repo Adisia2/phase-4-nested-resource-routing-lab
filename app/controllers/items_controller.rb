bdc5629042cd6c934400f96b197fe63949ce8921
+John <+>
2023-03-26 15:10:54 +0300

    complete assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
+# app/controllers/items_controller.rb
 class ItemsController < ApplicationController
+  before_action :set_user
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    @items = @user.items
+    render json: @items
   end
 
+  def show
+    @item = @user.items.find(params[:id])
+    render json: @item
+  end
+
+  def create
+    @item = @user.items.build(item_params)
+
+    if @item.save
+      render json: @item, status: :created
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
