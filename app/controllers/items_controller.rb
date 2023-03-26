01e81ef1c01997aef115ce6ca03715a3e8eaae32
+John <+>
2023-03-26 15:30:24 +0300

    complete assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,19 @@
 # app/controllers/items_controller.rb
 class ItemsController < ApplicationController
-  before_action :set_user
+  before_action :set_user, except: [:index]
 
   def index
-    @items = @user.items
-    render json: @items
+    @items = Item.includes(:user).where(user_id: params[:user_id])
+    render json: @items, include: :user
   end
 
   def show
-    @item = @user.items.find(params[:id])
-    render json: @item
+    @item = @user.items.find_by(id: params[:id])
+    if @item
+      render json: @item
+    else
+      render json: { error: "Item not found" }, status: :not_found
+    end
   end
 
   def create
@@ -18,17 +22,21 @@ class ItemsController < ApplicationController
     if @item.save
       render json: @item, status: :created
     else
-      render json: @item.errors, status: :unprocessable_entity
+      render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
   def set_user
-    @user = User.find(params[:user_id])
+    begin
+      @user = User.find(params[:user_id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found' }, status: :not_found
+    end
   end
 
   def item_params
-    params.require(:item).permit(:name, :description, :price)
+    params.permit(:name, :description, :price).merge(user_id: params[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
   resources :items, only: [:index, :show, :create]
   end
+
+  get '/items', to: 'items#index'
 end
